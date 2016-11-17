c1ace8228fa5aad3f7229fa6ad8eac19176a04d9
pedroromn <pedroromn@gmail.com>
2016-11-17 16:25:42 -0500

    17/112016 16:25

---
+++ pycode/datetime_webserver.py
@@ -0,0 +1,21 @@
+#! -*- coding: utf-8 -*-
+
+import time
+
+def print_header(title):
+    print """Content-type: text/html
+
+    <?xml version = "1.0" encoding = "UTF-8"?>
+    <!DOCTYPE html>
+    <html>
+    <head><title>%s</title></head>
+
+    <body>""" % title
+
+def main():
+    print_header("Current data and time")
+    print time.ctime( time.time() )
+    print "</body></html>"
+
+if __name__ == '__main__':
+    main()

c27bd7082d326b77d5b35c47c6d69a8c2d70e814
pedroromn <pedroromn@gmail.com>
2017-10-20 17:51:04 -0500

    20/10/2017 17:51

---
+++ pyds/stack_test.py
@@ -0,0 +1,13 @@
+#! -*- coding: utf-8 -*- 
+
+from __future__ import print_function
+from stack import Stack
+
+
+def main():
+    s = Stack()
+    print(s.is_empty())
+    
+
+if __name__ == "__main__":
+    main()

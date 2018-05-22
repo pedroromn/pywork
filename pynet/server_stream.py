21033ec137f79bd8119ea6c2941e0502646b2c27
pedroromn <user@localhost.com>
2018-05-22 12:20:41 -0500

    22/05/2018 12:20

--- pynet/server_stream.py
+++ pynet/server_stream.py
@@ -8,9 +8,11 @@ import socket
 
 HOST = "127.0.0.1"
 PORT = 5000
+MAX_BYTES = 1024
 counter = 0
 
 # step 1: create a socket
+print "Waiting for connection"
 socket_server = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 
 # step 2: bind the socket
@@ -28,7 +30,7 @@ while 1:
 
     # step 5: process connection
     socket_conn.send("SERVER>>> Connection successful")
-    client_message = socket_conn.recv(1024)
+    client_message = socket_conn.recv(MAX_BYTES)
 
     while client_message != "CLIENT>>> TERMINATE":
 
@@ -38,7 +40,7 @@ while 1:
         print client_message
         server_message = raw_input("SERVER>>> ")
         socket_conn.send("SERVER>>> " + server_message)
-        client_message = socket_conn.recv(1024)
+        client_message = socket_conn.recv(MAX_BYTES)
 
     print "Connection terminated"
     socket_conn.close()

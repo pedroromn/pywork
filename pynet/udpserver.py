90b7920a752fb1aac0708ed8bd22e935e133ffeb
pedroromn <user@localhost.com>
2018-05-29 15:05:26 -0500

    29/05/2018 15:05

--- pynet/udpserver.py
+++ pynet/udpserver.py
@@ -11,3 +11,23 @@ PORT = 5000
 server_socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 
 # step 2 : bind the socket
+server_socket.bind( (HOST, PORT) )
+
+while 1:
+
+    # step 3: receive packet
+    packet, address = server_socket.recvfrom( 1024 )
+
+    print "Packet received"
+    print "From host: ", address[0]
+    print "Host port: ", address[1]
+    print "Length: ", len(packet)
+    print "Containing: "
+    print "\t" + packet
+
+    # step 4: echo packet back to client
+    print "\nEcho data to client...",
+    socket_server.sendto( packet, address )
+    print "Packet sent\n"
+
+server_socket.close()

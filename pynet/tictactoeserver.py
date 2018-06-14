7a4118a3962dbb64174f138cc4d1f2364a642fbb
pedroromn <user@localhost.com>
2018-06-14 17:51:25 -0500

    14/06/2018 17:51

--- pynet/tictactoeserver.py
+++ pynet/tictactoeserver.py
@@ -27,3 +27,51 @@ class Player( threading.Thread ):
         
         self.connection.send("Opponent moved.")
         self.connection.send( str(location) )
+
+    def run( self ):
+        "Play the game"
+
+        self.server.display( "Player %s connected " % self.mark )
+        self.connection.send( self.mark )
+        self.connection.send( "%s connected " % self.mark )
+
+        # wait for another player to arrive
+        if self.mark == 'X':
+            self.connected.send( "Waiting for another player... " )
+            self.server.game_begin_event.wait()
+            self.connection.send("Other player connected. Your move.")
+        else:
+            self.server.game_begin_event.wait()
+            self.connection.send( "Waiting for first move..." )
+
+        # play game
+        while not self.server.game_over():
+            location = self.connection.recv(2)
+
+            if not location:
+                break
+            
+            if self.server.valid_move( int(location), self.number ):
+                self.server.display( "loc: " + location )
+                self.connection.send( "Valid move." )
+            else:
+                self.connection.send("Invalid move, try again.")
+
+        self.connection.close()
+        self.server.display("Game Over")
+        self.server.display("Connection closed")
+
+class TicTacToeServer(object):
+    "Server that mantains a game of TicTacToe for two clients"
+
+    def __init__( self ):
+        "Initialize variables and setup server"
+
+        HOST = ""
+        PORT = 5000
+
+        self.board = []
+        self.current_player = 0
+        self.turn_condition = threading.Condition()
+        self.game_begin_event = threading.Event()
+

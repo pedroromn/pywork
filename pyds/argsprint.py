5fba079bdfe36ea9ba9054beb1d4cbe996531924
pedroromn <pedroromn@gmail.com>
2018-07-08 00:53:49 -0500

    08/07/2018 00:53

---
+++ pyds/argsprint.py
@@ -0,0 +1,8 @@
+#! -*- coding: utf-8 -*-
+
+import sys
+
+print "This is the name of the script: {}".format(sys.argv[0])
+print "Number of arguments: {}".format(len(sys.argv))
+print "The arguments are: {}".format(str(sys.argv))
+print""

90ec2596d035ea38633715a9e3c0b359e2578926
pedroromn <pedroromn@gmail.com>
2019-10-08 20:12:00 -0500

    08/10/2019 20:11

--- pyds/main_craps.py
+++ pyds/main_craps.py
@@ -6,20 +6,20 @@ from craps import Player
 def play_one_game():
     player = Player()
     you_win = player.play()
-    print player
+    print(player)
     if you_win :
-        print "You win!"
+        print("You win!")
     else:
-        print "You lose!"
+        print("You lose!")
 
 def play_many_games():
-    number = input("Enter the number of games: ")
+    number = int(input("Enter the number of games: "))
     wins = 0
     losses = 0
     win_rolls = 0
     loss_rolls = 0
     player = Player()
-    for count in xrange(number):
+    for count in range(number):
         has_won = player.play()
         rolls = player.get_number_rolls()
         if has_won:
@@ -28,12 +28,12 @@ def play_many_games():
         else:
             losses += 1
             loss_rolls += rolls
-    print "The total number of wins is ", wins
-    print "The total number of losses is ", losses
-    print "The average number of rolls per win is %0.2f" % \
-            (float(win_rolls) / wins)
-    print "The winning percentage is %0.3f" % \
-            (float(wins) / number)
+    print("The total number of wins is ", wins)
+    print("The total number of losses is ", losses)
+    print ("The average number of rolls per win is %0.2f" % \
+            (float(win_rolls) / wins))
+    print("The winning percentage is %0.3f" % \
+            (float(wins) / number))
 
 def main():
     #play_one_game()
